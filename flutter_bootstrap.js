9731f304abc5abe9969c02bfd84032a22022502e
SohJohAI <SohJohAI@users.noreply.github.com>
2025-03-29 01:46:18 +0000

    deploy: 1745a50cfdc1de1e28bc397a6fc70f3c2b2a71ce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "401414274"
+    serviceWorkerVersion: "1209259772"
   }
 });
